302126d3fd9a086927b452b24c90b63b3c637b21
Devel484 <p.kelbing96@web.de>
2021-01-28 22:05:37 +0100

    Initial websocket commit, sadly most examples are missing.

---
+++ examples/websocket.py
@@ -0,0 +1,17 @@
+from tradehub.websocket_client import DemexWebsocket
+import asyncio
+
+
+async def on_connect():
+    print("I am connect to demex websocket!")
+    await demex.subscribe_books("orderbook", "swth_eth1")
+
+
+async def on_receive_message(message: dict):
+    print("I received a message")
+    print(message)
+
+
+if __name__ == '__main__':
+    demex: DemexWebsocket = DemexWebsocket('ws://85.214.81.155:5000/ws')
+    asyncio.get_event_loop().run_until_complete(demex.connect(on_receive_message, on_connect))
